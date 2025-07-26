7c8805adfb10ca3c24067218f79f05df7cd5d4f5
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-26 15:28:33 +0300

    chore: scafold next app

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,13 @@
+ 
+
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <h1 className="h-screen w-full grid place-content-center text-4xl">Welcome!</h1>
+    </main>
+  )
+}
+
+
+export default Home;
